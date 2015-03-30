8d4f4e0f15d4b0d0ecb1fa7bc383428e436d61ad
Roman <roman.atachiants@gmail.com>
2015-03-30 13:46:27 +0100

    async

--- deploy.sh
+++ deploy.sh
@@ -11,12 +11,12 @@ export AWSSECRETACCESSKEY=$AWS_SECRET_KEY
 
 /usr/bin/s3fs -o allow_other -o use_cache=/tmp $BUCKET /data
 
-#set -e
-echo "/data *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+#echo "/data *(rw,sync,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
+echo "/data *(rw,async,no_subtree_check,fsid=0,no_root_squash)" >> /etc/exports
 
 chmod 777 /etc/sv/nfs/run
 chmod 777 /etc/sv/nfs/finish
 chmod 777 /etc/exports
+chmod 777 /data
 
-#exec runsvdir /etc/sv
 runsvdir /etc/sv
